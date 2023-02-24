cee4390e83e4d706ec1b5443bf8b9b69fbbcef2c
wangcong <wangcong@mampod.com>
2023-02-24 17:29:39 +0800

    root

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Editor from './editor'
 
 export default function Home() {
   return (
     <div className="container">
-      <h1>Hello World</h1>
+      <Editor />
     </div>
   )
 }
